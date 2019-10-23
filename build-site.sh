75b599dd0b4d71db4892e4057cfd4cb9689be6bf
George Mauer <gmauer@gmail.com>
2019-10-23 14:16:34 -0500

    fix up the build-site script to actually serve stuff too

--- build-site.sh
+++ build-site.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm --volume="$PWD:/srv/jekyll" -it jekyll/builder:3.8 jekyll build "$@"
+docker run --rm --volume="$(pwd):/srv/jekyll" --name "blog" -it -p 4000:4000 jekyll/builder:3.8 jekyll serve "$@"
 
