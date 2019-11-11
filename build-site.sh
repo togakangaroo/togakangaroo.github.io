d24f9b7c6a99f19f69e800e8e286e12d57cc02d4
George Mauer <gmauer@gmail.com>
2019-11-11 15:52:42 -0600

    swap readme markdown for an org

--- build-site.sh
+++ build-site.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm --volume="$(pwd):/srv/jekyll" --name "blog" -it -p 4000:4000 jekyll/builder:3.8 jekyll serve "$@"
+docker run --rm --volume="$(pwd):/srv/jekyll" --name "blog" -p 4000:4000 jekyll "$@"
 
